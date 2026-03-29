3e9aa36a04f462abd1324f7f732613290ce7d1c0
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2026-03-29 16:40:38 +0100

    refactor: Bump eslint from 7.32.0 to 9.39.3

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import babelParser from "@babel/eslint-parser";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 6,
+      sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+      },
+      globals: {
+        ...globals.node,
+        ...globals.es6,
+      },
+    },
+    rules: {
+      "indent": ["error", 2, { "SwitchCase": 1 }],
+      "linebreak-style": ["error", "unix"],
+      "no-trailing-spaces": 2,
+      "eol-last": 2,
+      "space-in-parens": ["error", "never"],
+      "no-multiple-empty-lines": 1,
+      "prefer-const": "error",
+      "space-infix-ops": "error",
+      "no-useless-escape": "off",
+      "require-atomic-updates": "off",
+    },
+  },
+  {
+    files: ["spec/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jasmine,
+      },
+    },
+  },
+];
